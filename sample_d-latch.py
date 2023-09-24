81bb77efce2c36fa394ea7d09f11666e507cdfab
janrutger <janrutger@jaruku.nl>
2023-09-24 11:22:06 +0200

    More Samples

--- sample_d-latch.py
+++ sample_d-latch.py
@@ -4,10 +4,10 @@ machine = m.LGM()
 nand2 = "(z AND a b)(c NOT z) c"
 machine.logic('nand2', nand2)
 
-Dlatch = "[(D E), (a b), nand2, (x)], [(E x), (a b), nand2, (y)], [(x Qn), (a b), nand2, (Q)], [(y Q), (a b), nand2, (Qn)]"
+Dlatch = "[(D E), (a b), nand2, (x)], [(E x), (a b), nand2, (y)], [(x Qn), (a b), nand2, (Q)], [(y Q), (a b), nand2, (Qn)], [(x Qn), (a b), nand2, (Q)], [(y Q), (a b), nand2, (Qn)]"
 
 machine.chip('Dlatch', Dlatch)
-machine.dip('10', "(Q Qn)")
+#machine.dip('10', "(Q Qn)")
 
 burn = "(D E)(Dlatch)(Q Qn)"
 machine.burn("CHIP", burn)
