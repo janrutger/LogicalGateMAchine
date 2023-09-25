2a00ea4da732aebd6d62f248011aa610e220c78e
janrutger <janrutger@jaruku.nl>
2023-09-25 20:24:43 +0200

    Firdt version of mine 1bit ALU

--- sample_ALU.py
+++ sample_ALU.py
@@ -1,17 +1,13 @@
 import lgm as m
 machine = m.LGM()
-
+## All needed logic
 plexer21 = "(z NOT X)(y AND D1 X)(x AND D0 z)(O OR x y) O"
 machine.logic('plexer21', plexer21)
 
-# plexer = "(z NOT S1)(y AND D1 S1)(x AND D0 z)(r OR x y)(z1 NOT S2)(y1 AND D2 S2)(x1 AND r z1)(O OR x1 y1) O"
 plexer31 = "(z NOT X1)(y AND D1 X1)(x AND D0 z)(r OR x y)(z1 NOT X2)(y1 AND D2 X2)(x1 AND r z1)(O OR x1 y1) O"
-# [(r0 r1 r2 S1 S2), (D0 D1 D2 X1 X2), plexer31, (O0)]
 machine.logic('plexer31', plexer31)
 
 adder = "(x XOR A B)(z AND A B)(S XOR x Cx)(y AND x Cx)(Co OR z y) Co S"
-#[(Ain Bin Cin), (A B Cx), adder, (Cout r2)]
-
 machine.logic('adder', adder)
 
 and2 = "(c AND a b) c"
@@ -24,11 +20,17 @@ not1 = "(b NOT a) b"
 machine.logic('not1', not1)
 
 
+
+## Glue the logic to on bit ALU
 ALU = "[(A0), (a), not1, (An)], [(B0), (a), not1, (Bn)], [(A0 An Anot), (D0 D1 X), plexer21, (Ain)], [(B0 Bn Bnot), (D0 D1 X), plexer21, (Bin)], [(Ain Bin Cin), (A B Cx), adder, (Cout r2)], [(Ain Bin), (a b), or2, (r1)], [(Ain Bin), (a b), and2, (r0)], [(r0 r1 r2 S1 S2), (D0 D1 D2 X1 X2), plexer31, (O0)]"
 machine.chip('ALU', ALU)
 
-machine.dip('10', "(A0 B0)")
-machine.dip('000', "(Anot Bnot Cin)")
+
+
+
+## Run de ALU
+machine.dip('01', "(A0 B0)")
+machine.dip('011', "(Anot Bnot Cin)")
 machine.dip('01', "(S1 S2)")
 
 machine.run("ALU")
