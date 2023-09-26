a3103fb6b1640ff2fdaa8f8232989a2c9e8d9f64
janrutger <janrutger@jaruku.nl>
2023-09-26 17:49:34 +0200

    Unittest for  ALU

--- test_ALU.py
+++ test_ALU.py
@@ -46,6 +46,70 @@ class TestGateLogic(unittest.TestCase):
         self.machine.run('ALU')
         self.assertEqual(self.machine.led('(O0)'), "1")
 
+    def test_NAND_gate(self):
+        self.machine.dip('110', "(Anot Bnot Cin)")
+        self.machine.dip('01', "(S2 S1)")
+
+
+        self.machine.dip('00', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "1")
+
+        self.machine.dip('01', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "1")
+
+        self.machine.dip('10', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "1")
+
+        self.machine.dip('11', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "0")
+
+    def test_OR_gate(self):
+        self.machine.dip('000', "(Anot Bnot Cin)")
+        self.machine.dip('01', "(S2 S1)")
+
+
+        self.machine.dip('00', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "0")
+
+        self.machine.dip('01', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "1")
+
+        self.machine.dip('10', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "1")
+
+        self.machine.dip('11', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "1")
+
+    def test_NOR_gate(self):
+        self.machine.dip('110', "(Anot Bnot Cin)")
+        self.machine.dip('00', "(S2 S1)")
+
+
+        self.machine.dip('00', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "1")
+
+        self.machine.dip('01', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "0")
+
+        self.machine.dip('10', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "0")
+
+        self.machine.dip('11', "(A0 B0)")
+        self.machine.run('ALU')
+        self.assertEqual(self.machine.led('(O0)'), "0")
+
+
 
 
 
