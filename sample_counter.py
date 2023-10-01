320179291d5db04b12b29f695dbc09eba0f8ff41
janrutger <janrutger@jaruku.nl>
2023-10-01 21:29:50 +0200

    4 bits counter

--- sample_counter.py
+++ sample_counter.py
@@ -1,35 +1,55 @@
-# An Johnson-Counter (ring-countr)
-
 import lgm as m
+
 machine = m.LGM()
 
+#CLOCK and Clock2
+logic = "(O AND A On)(On NOT O) O On"
+machine.logic('logic', logic)
+chip = "[(E), (A), logic, (Clk Clkn)]"
+machine.chip('clock', chip)
+
+clk1 = "(clk1 AND Clk Q) clk1"
+machine.logic('clk1', clk1)
+
+clk2 = "(clk2 AND clk1 Q1) clk2"
+machine.logic('clk2', clk2)
+
+#D-latch
 nand2 = "(z AND a b)(c NOT z) c"
 machine.logic('nand2', nand2)
 
-Dlatch1 ="[(Qn5 E), (a b), nand2, (x)], [(E x), (a b), nand2, (y)], [(x Qn1), (a b), nand2, (Q1)], [(y Q1), (a b), nand2, (Qn1)], [(x Qn1), (a b), nand2, (Q1)], [(y Q1), (a b), nand2, (Qn1)]"
-Dlatch2 = "[(Q1 E), (a b), nand2, (x)], [(E x), (a b), nand2, (y)], [(x Qn2), (a b), nand2, (Q2)], [(y Q2), (a b), nand2, (Qn2)], [(x Qn2), (a b), nand2, (Q2)], [(y Q2), (a b), nand2, (Qn2)]"
-Dlatch3 = "[(Q2 E), (a b), nand2, (x)], [(E x), (a b), nand2, (y)], [(x Qn3), (a b), nand2, (Q3)], [(y Q3), (a b), nand2, (Qn3)], [(x Qn3), (a b), nand2, (Q3)], [(y Q3), (a b), nand2, (Qn3)]"
-Dlatch4 = "[(Q3 E), (a b), nand2, (x)], [(E x), (a b), nand2, (y)], [(x Qn4), (a b), nand2, (Q4)], [(y Q4), (a b), nand2, (Qn4)], [(x Qn4), (a b), nand2, (Q4)], [(y Q4), (a b), nand2, (Qn4)]"
-Dlatch5 = "[(Q4 E), (a b), nand2, (x)], [(E x), (a b), nand2, (y)], [(x Qn5), (a b), nand2, (Q5)], [(y Q5), (a b), nand2, (Qn5)], [(x Qn5), (a b), nand2, (Q5)], [(y Q5), (a b), nand2, (Qn5)]"
+dlatch = "[(Qn Clk), (a b), nand2, (x)], [(Clk x), (a b), nand2, (y)], [(x Qn), (a b), nand2, (Q)], [(y Q), (a b), nand2, (Qn)], [(x Qn), (a b), nand2, (Q)], [(y Q), (a b), nand2, (Qn)]"
+machine.chip('dlatch', dlatch)
 
-machine.chip('Dlatch1', Dlatch1)
-machine.chip('Dlatch2', Dlatch2)
-machine.chip('Dlatch3', Dlatch3)
-machine.chip('Dlatch4', Dlatch4)
-machine.chip('Dlatch5', Dlatch5)
+dlatch1 = "[(Qn1 clk1), (a b), nand2, (x)], [(clk1 x), (a b), nand2, (y)], [(x Qn1), (a b), nand2, (Q1)], [(y Q1), (a b), nand2, (Qn1)], [(x Qn1), (a b), nand2, (Q1)], [(y Q1), (a b), nand2, (Qn1)]"
+machine.chip('dlatch1', dlatch1)
 
-burn = "(E)(Dlatch5 Dlatch4 Dlatch3 Dlatch2 Dlatch1)(Q4 Q3 Q2 Q1)"
+dlatch2 = "[(Qn2 clk2), (a b), nand2, (x)], [(clk2 x), (a b), nand2, (y)], [(x Qn2), (a b), nand2, (Q2)], [(y Q2), (a b), nand2, (Qn2)], [(x Qn2), (a b), nand2, (Q2)], [(y Q2), (a b), nand2, (Qn2)]"
+machine.chip('dlatch2', dlatch2)
+
+
+burn = "(E)(clock dlatch clk1 dlatch1 clk2 dlatch2)(Qn2 Qn1 Qn Clkn)"
 machine.burn("counter", burn)
 
-print(machine.run("counter", ("0")))
-print(machine.run("counter", ("1")))
-print(machine.run("counter", ("1")))
-print(machine.run("counter", ("1")))
-print(machine.run("counter", ("1")))
-print(machine.run("counter", ("1")))
-print(machine.run("counter", ("1")))
-print(machine.run("counter", ("1")))
-print(machine.run("counter", ("1")))
+## Run logic
+
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
+print(machine.run('counter', ("1")))
 
 
-#print(machine.model())
