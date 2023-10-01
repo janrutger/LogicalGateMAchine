9176f90d019830356aac80ca872c557f517d7538
janrutger <janrutger@jaruku.nl>
2023-10-01 20:06:37 +0200

    Divider 0.2 3 bits

--- sample_divider.py
+++ sample_divider.py
@@ -7,8 +7,6 @@ logic = "(O AND A On)(On NOT O) O On"
 machine.logic('logic', logic)
 chip = "[(E), (A), logic, (Clk Clkn)]"
 machine.chip('clock', chip)
-# burn = "(E)(clock)(Clk Clkn)"
-# machine.burn("Clock", burn)
 
 logic = "(clk1 AND Clk Q) clk1"
 machine.logic('clk1', logic)
@@ -20,13 +18,10 @@ machine.logic('nand2', nand2)
 
 dlatch = "[(Qn Clk), (a b), nand2, (x)], [(Clk x), (a b), nand2, (y)], [(x Qn), (a b), nand2, (Q)], [(y Q), (a b), nand2, (Qn)], [(x Qn), (a b), nand2, (Q)], [(y Q), (a b), nand2, (Qn)]"
 machine.chip('dlatch', dlatch)
-#burn = "(Clk)(dlatch)(Q)"
-#machine.burn("Dlatch", burn)
 
 dlatch1 = "[(Qn1 clk1), (a b), nand2, (x)], [(clk1 x), (a b), nand2, (y)], [(x Qn1), (a b), nand2, (Q1)], [(y Q1), (a b), nand2, (Qn1)], [(x Qn1), (a b), nand2, (Q1)], [(y Q1), (a b), nand2, (Qn1)]"
 machine.chip('dlatch1', dlatch1)
-#burn1 = "(Q)(dlatch1)(Q1)"
-#machine.burn("Dlatch1", burn1)
+
 
 burn = "(E)(clock dlatch clk1 dlatch1)(Qn1 Qn Clkn)"
 machine.burn("divider", burn)
@@ -42,87 +37,7 @@ print(machine.run('divider', ("1")))
 print(machine.run('divider', ("1")))
 print(machine.run('divider', ("1")))
 
-
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-# machine.run('Dlatch', (machine.run('Clock', ("1"))))
-# machine.run('Clk2')
-# machine.run('dlatch1')
-# print(machine.led("(Qn1 Qn Clkn)"))
-
-
-
-
-
+print(machine.run('divider', ("1")))
 
 
 
